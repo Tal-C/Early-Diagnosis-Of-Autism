bfd2915a173d44e87e0ec1b731dbc17cad8e63d8
Tal-C <tal3433@gmail.com>
2016-12-05 14:39:20 +0200

    Update

--- EDIA/EDIA/FlaskApp/app.py
+++ EDIA/EDIA/FlaskApp/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.route('/')
 def main():
-    return render_template('Index.html')
+    return render_template('index.html')
 
 
 
