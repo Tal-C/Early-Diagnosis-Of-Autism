dbc78b2c09a67c558967cee1272d704de1020ffa
katiapr <katiapr89@gmail.com>
2016-12-07 15:12:46 +0200

    update 71216

--- EDIA/EDIA/FlaskApp/app.py
+++ EDIA/EDIA/FlaskApp/app.py
@@ -1,13 +1,23 @@
 ﻿
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request,redirect
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
+    return render_template('index.html')
+
+@app.route('/item.html', methods=['GET', 'POST'])
+def item():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+      # show the form, it wasn't submitted
     return render_template('item.html')
-
-
+        
 
 if __name__ == "__main__":
     app.run()
