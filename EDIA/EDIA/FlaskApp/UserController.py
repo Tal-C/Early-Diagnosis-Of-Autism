9778ca9b608d407158ebe6bfbf62d4dba6fb8b19
katiapr <katiapr89@gmail.com>
2017-01-12 19:30:35 +0200

    Login - 120117

---
+++ EDIA/EDIA/FlaskApp/UserController.py
@@ -0,0 +1,12 @@
+﻿import MySQLconnection
+
+class UserController():
+
+    def __init__(self):
+        return
+
+    def login_page(userName,password):
+        ##reg exp
+        ##send to MySQLConnector
+        print("the beach is beautiful!")
+       
--- EDIA/EDIA/FlaskApp/app.py
+++ EDIA/EDIA/FlaskApp/app.py
@@ -1,5 +1,14 @@
 ﻿
 from flask import Flask, render_template, url_for,request,redirect
+import requests
+#from lxml import html
+#import cgi   
+from flask.helpers import flash
+import gc
+from UserController import UserController
+
+
+
 app = Flask(__name__)
 
 
@@ -9,13 +18,25 @@ def main():
 
 @app.route('/item.html', methods=['GET', 'POST'])
 def item():
-    if request.method == 'POST':
-        # do stuff when the form is submitted
-        # redirect to end the POST handling
-        # the redirect can be to the same route or somewhere else
-        return redirect(url_for('item'))
-      # show the form, it wasn't submitted
-    return render_template('item.html')
+    error = ''
+    try:
+        if request.method == 'POST':
+            attempted_username = request.form['user']
+            attempted_password = request.form['passw']
+            print("userName = ",attempted_username)
+            print("Password = ",attempted_password)
+            resp = UserController.login_page(attempted_username,attempted_password)
+            if resp == 0:
+                flash("You are now logged in!")
+                return render_template("item.html")
+            else:
+                error = "Invalid credetials,try again .!. "
+            gc.collect()
+    except Exception as e:
+        return render_template("item.html",error = error)
+        #return redirect(url_for('item'))
+    # show the form, it wasn't submitted
+    return render_template('index.html')
         
 
 if __name__ == "__main__":
