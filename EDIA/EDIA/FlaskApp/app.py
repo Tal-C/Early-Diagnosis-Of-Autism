7dda886e7b4176ab028fdfe9e881e6d3cc198235
Tal-C <tal3433@gmail.com>
2017-01-30 17:09:58 +0200

    Integration 300117 1709

--- EDIA/EDIA/FlaskApp/app.py
+++ EDIA/EDIA/FlaskApp/app.py
@@ -22,8 +22,6 @@ def item():
             if attempted_username == '' and attempted_password == '':
                 print("you must sing up")
             else:
-                print("userName = ", attempted_username)
-                print("Password = ", attempted_password)
                 resp = UserController.login_page(attempted_username,attempted_password)
                 if resp == 0:
                     print("You are now logged in!")
