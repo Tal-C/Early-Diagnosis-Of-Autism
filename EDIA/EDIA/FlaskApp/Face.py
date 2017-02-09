59194b471b780e703037a86b9da47e945d30dfba
Tal-C <tal3433@gmail.com>
2017-02-09 16:19:58 +0200

    Update 090217 1619

--- EDIA/EDIA/FlaskApp/Face.py
+++ EDIA/EDIA/FlaskApp/Face.py
@@ -70,7 +70,7 @@ class Face(object):
         eye_rect = self.img[y1:y2, x1:x2]
 
         #eyes1 = d.get('haarcascade_mcs_eyepair_big').detectMultiScale(self.eye_rect, 1.2, 1)
-        eyes_cascade = cv2.CascadeClassifier('C:\Users\Katia\Anaconda3\pkgs\opencv-3.1.0-np111py35_1\Library\etc\haarcascades\haarcascade_frontalface_default.xml')
+        eyes_cascade = cv2.CascadeClassifier('C:\\Users\\tal\\Downloads\\opencv\\sources\\data\\haarcascades\\haarcascade_eye.xml')
         eyes = eyes_cascade.detectMultiScale(eye_rect, 1.1, 1)
         for eye in eyes:
             eye[0] += x1
@@ -151,7 +151,7 @@ class Face(object):
         N = 6
         x1,y1,x2,y2 = self.nose_rect(N) 
         nose_rect = self.img[y1:y2, x1:x2]    
-        nose_cascade = cv2.CascadeClassifier('C:\Users\Katia\Anaconda3\pkgs\opencv-3.1.0-np111py35_1\Library\etc\haarcascades\haarcascade_frontalface_default.xml')
+        nose_cascade = cv2.CascadeClassifier('C:\\Users\\tal\\Downloads\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml')
         noses = nose_cascade.detectMultiScale(nose_rect, 1.2, 1)
         distance = self.h
         s = self.w*self.h
@@ -196,7 +196,7 @@ class Face(object):
     def smile_detection(self):
         x1,y1,x2,y2 = self.mouth_rect()
         mouth_rect = self.img[y1:y2, x1:x2]
-        smile_cascade = face_cascade = cv2.CascadeClassifier('C:\Users\Katia\Anaconda3\pkgs\opencv-3.1.0-np111py35_1\Library\etc\haarcascades\haarcascade_frontalface_default.xml')
+        smile_cascade = face_cascade = cv2.CascadeClassifier('C:\\Users\\tal\\Downloads\\opencv\\sources\\data\\haarcascades\\haarcascade_smile.xml')
         smiles = smile_cascade.detectMultiScale(mouth_rect, 1.1, 4)
         #mouthes = d.get('haarcascade_mcs_mouth').detectMultiScale(mouth_rect, 1.5, 2)
         i = None
